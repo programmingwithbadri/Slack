1280d8a104d4455bd1256476fdcf8f5b52766d04
Badri <badri.py@outlook.com>
2020-04-18 20:47:55 +0530

    Update the DOM with the namespace info

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,20 @@
 // Connect to default or main ('/') socket namespace
-const socket = io("http://localhost:9000"); 
+const socket = io("http://localhost:9000");
 
 // Listen for all the available namespaces
 socket.on("nsList", (nsData) => {
-  console.log(nsData)
-})
+  // Update the dom element with the namespace image
+  let nsDiv = document.querySelector(".namespaces");
+  nsDiv.innerHTML = "";
+  nsData.forEach((ns) => {
+    nsDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src ="${ns.img}"/></div>`;
+  });
+
+  // Add the click listener for the NS
+  Array.from(document.getElementsByClassName("namespace")).forEach((nsElem) => {
+    nsElem.addEventListener("click", (e) => {
+      const nsEndpoint = nsElem.getAttribute("ns");
+      console.log(`Navigating to ${nsEndpoint}...`)
+    });
+  });
+});
