81c14cdc11675b1e7f90d7b459fbbe9a78240b6d
Badri <badri.py@outlook.com>
2020-04-19 17:37:48 +0530

    Implement search box to search text

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -20,4 +20,20 @@ function joinRoom(roomName) {
     ).innerHTML = `${noOfMembers}<span class="glyphicon glyphicon-user"></span>`;
     document.querySelector(".curr-room-text").innerText = `${roomName}`;
   });
+
+  let searchBox = document.querySelector("#search-box");
+  searchBox.addEventListener("input", (e) => {
+    let messages = Array.from(document.getElementsByClassName("message-text"));
+    console.log(messages);
+    messages.forEach((msg) => {
+      if (
+        msg.innerText.toLowerCase().indexOf(e.target.value.toLowerCase()) === -1
+      ) {
+        // the msg does not contain the user search term!
+        msg.style.display = "none";
+      } else {
+        msg.style.display = "block";
+      }
+    });
+  });
 }
