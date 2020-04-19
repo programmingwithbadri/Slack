9a99f61b3444a382739e874cbdd712c456be5120
Badri <badri.py@outlook.com>
2020-04-19 16:17:02 +0530

    Pass the message to all the clients in the room

--- slack.js
+++ slack.js
@@ -49,7 +49,12 @@ namespaces.forEach((namespace) => {
 
     // Listen for the messages emitted by client
     nsSocket.on("newMessageToServer", (msg) => {
-      console.log(msg)
+      // Pass the received msg to all the clients that are in the room that this socket is in
+      // the user will be in the 2nd room in the object list
+      // because the socket ALWAYS joins its own room on connection(which is 1st index in the object list)
+      // get the keys
+      const roomTitle = Object.keys(nsSocket.rooms)[1];
+      io.of("/wiki").to(roomTitle).emit("messageToClients", msg);
     });
   });
 });
