42048756627bc7bda6a62d5a72050c23ed7fc4cd
Badri <badri.py@outlook.com>
2020-04-19 16:46:12 +0530

    Get the msg history of the room from the server

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -3,6 +3,19 @@ function joinRoom(roomName) {
   nsSocket.emit("joinRoom", roomName, (noOfMembers) => {
     // Update the number of memebers in the room once joined
     // noOfMembers - ack we got from the server once the event is emitted
-    document.querySelector('.curr-room-num-users').innerHTML = `${noOfMembers}<span class="glyphicon glyphicon-user"></span>`
+    document.querySelector(
+      ".curr-room-num-users"
+    ).innerHTML = `${noOfMembers}<span class="glyphicon glyphicon-user"></span>`;
+  });
+
+  // Get the msg history of the room from the server
+  nsSocket.on("historyCatchup", (history) => {
+    const msgUl = document.querySelector("#messages");
+    msgUl.innerHTML = "";
+    history.forEach((msg) => {
+      const newMsg = buildHTML(msg);
+      const currentMsgs = msgUl.innerHTML;
+      msgUl.innerHTML = currentMsgs + newMsg;
+    });
   });
 }
